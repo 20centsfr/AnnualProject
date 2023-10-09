336ec2808189bab261962761bea83391ebe40328
Anthony BAYRAKTAR <89924773+20centsfr@users.noreply.github.com>
2023-10-09 16:28:24 +0200

    Update db

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,7 @@
 <?php
-/*
-try{
-	$db = new PDO('mysql:host=localhost;port=3306;dbname=ts', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-}catch(Exception $e){
-	die('Erreur : ' . $e->getMessage()); 
-}
-
-*/
 
 try {
-	$db = new PDO('mysql:host=64.226.72.114;port=3306;dbname=ts', 'together', 'projetannuel', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+	$db = new PDO('mysql:host=..;port=3306;dbname=..', '..', '..', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch(Exception $e) {
 	die('Erreur : ' . $e->getMessage()); 
 }
